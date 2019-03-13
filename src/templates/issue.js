c2dd341c0fcdd8f4b2039fddd2764b98a39b1880
Frederick Bejarano Sanchez <ingfrederick@gmail.com>
2019-03-13 18:39:08 -0300

    fix discovery and issues

--- src/templates/issue.js
+++ src/templates/issue.js
@@ -14,74 +14,117 @@ import { MetaData } from '../components/common/meta'
 const Issue = ({ data, location }) => {
     const post = data.ghostPost
     const url = `${post.slug}/contribute/tier/${post.ghostId}-backers`
+    const tabs = [
+        { active: true, text: `Qué` },
+        { active: true, text: `Por qué` },
+        { active: false, text: `Cuando` },
+        { active: false, text: `Dónde` },
+        { active: false, text: `Quién` },
+        { active: false, text: `Cómo` },
+    ]
+
     return (
-            <>
-                <MetaData
-                    data={data}
-                    location={location}
-                    type="article"
-                />
-                <Layout>
-                    <section className="hero is-medium">
-                        <div className="hero-body">
-                            <div className="container has-text-centered">
-                                <figure className="image is-64x64" style={{ margin: `0 auto` }}>
-                                    <img className="is-issue-icon" src={AdoptaUnHincha}/>
-                                </figure>
-                                <h1 className="title is-spaced is-size-2-desktop is-size-3-mobile">{post.title}</h1>
-                                <h2 className="subtitle is-size-5-desktop is-size-5-mobile">{post.primary_author.bio}</h2>
-                                <h2 className="subtitle is-size-6-desktop is-size-5-mobile">Hosted by <strong>{post.primary_author.name}</strong></h2>
-                                <div className="backers-icons">
-                                    <p className="field">
-                                        <a className="button is-warning is-medium" href="">
-                                            <span className="icon is-large">
-                                                <figure className="image" style={{ marginLeft: `2px` }}>🐱</figure>
-                                            </span>
-                                        </a>
-                                        <a className="button is-danger is-medium" href="">
-                                            <span className="icon is-large">
-                                                <figure className="image" style={{ marginLeft: `2px` }}>🐷</figure>
-                                            </span>
-                                        </a>
-                                        <a className="button is-success is-medium" href="">
-                                            <span className="icon is-large">
-                                                <figure className="image" style={{ marginLeft: `2px` }}>🐨</figure>
-                                            </span>
-                                        </a>
-                                    </p>
-                                </div>
-                                <hr className="hr" style={{ backgroundColor: `transparent`, marginBottom: `1.5rem` }}/>
-                                <h2 className="subtitle is-size-6-desktop is-size-5-mobile">Gracias a sus contribuciones financieras, estamos operando con un presupuesto anual de marketing estimado de <strong>ARS $68.732</strong></h2>
-                                <progress className="progress is-small is-primary" value="54.94" max="100">54.94%</progress>
-                                <Link to={url} className="button is-large is-primary is-rounded">Apoyar</Link>
+        <>
+            <MetaData
+                data={data}
+                location={location}
+                type="article"
+            />
+            <Layout heroBody={HeroBody} tabs={tabs}
+                navColor="white"
+                title={post.title}
+                excerpt={post.excerpt}
+                host={post.primary_author.name}
+                modifiers={`is-medium has-text-centered is-primary`}>
+
+                <div className="section">
+                    <div className="container">
+                        <div className="box">
+                            <div className="content" dangerouslySetInnerHTML={{ __html: post.html }}>
                             </div>
                         </div>
-                        {/* <!-- Hero footer: will stick at the bottom --> */}
-                        <div className="hero-foot">
-                            <nav className="tabs">
-                                <div className="container">
-                                    <ul>
-                                        <li className="is-active"><a>Overview</a></li>
-                                        <li><a>Modifiers</a></li>
-                                        <li><a>Grid</a></li>
-                                        <li><a>Elements</a></li>
-                                        <li><a>Components</a></li>
-                                        <li><a>Layout</a></li>
-                                    </ul>
-                                </div>
-                            </nav>
-                            <section className="box issue-body">
-                                <div className="container is-narrow">
-                                    <div className="content" dangerouslySetInnerHTML={{ __html: post.html }}>
-                                    </div>
-                                </div>
-                            </section>
-                        </div>
-                    </section>
-                </Layout>
-            </>
+                    </div>
+                </div>
+            </Layout>
+        </>
     )
+
+    // return (
+    //         <>
+    //             <MetaData
+    //                 data={data}
+    //                 location={location}
+    //                 type="article"
+    //             />
+    //             <Layout>
+    //                 <section className="hero is-medium">
+    //                     <div className="hero-body">
+    //                         <div className="container has-text-centered">
+    //                             <figure className="image is-64x64" style={{ margin: `0 auto` }}>
+    //                                 <img className="is-issue-icon" src={AdoptaUnHincha}/>
+    //                             </figure>
+    //                             <h1 className="title is-spaced is-size-2-desktop is-size-3-mobile">{post.title}</h1>
+    //                             <h2 className="subtitle is-size-5-desktop is-size-5-mobile">{post.primary_author.bio}</h2>
+    //                             <h2 className="subtitle is-size-6-desktop is-size-5-mobile">Hosted by <strong>{post.primary_author.name}</strong></h2>
+    //                             <div className="backers-icons">
+    //                                 <p className="field">
+    //                                     <a className="button is-warning is-medium" href="">
+    //                                         <span className="icon is-large">
+    //                                             <figure className="image" style={{ marginLeft: `2px` }}>🐱</figure>
+    //                                         </span>
+    //                                     </a>
+    //                                     <a className="button is-danger is-medium" href="">
+    //                                         <span className="icon is-large">
+    //                                             <figure className="image" style={{ marginLeft: `2px` }}>🐷</figure>
+    //                                         </span>
+    //                                     </a>
+    //                                     <a className="button is-success is-medium" href="">
+    //                                         <span className="icon is-large">
+    //                                             <figure className="image" style={{ marginLeft: `2px` }}>🐨</figure>
+    //                                         </span>
+    //                                     </a>
+    //                                 </p>
+    //                             </div>
+    //                             <hr className="hr" style={{ backgroundColor: `transparent`, marginBottom: `1.5rem` }}/>
+    //                             <h2 className="subtitle is-size-6-desktop is-size-5-mobile">Gracias a sus contribuciones financieras, estamos operando con un presupuesto anual de marketing estimado de <strong>ARS $68.732</strong></h2>
+    //                             <progress className="progress is-small is-primary" value="54.94" max="100">54.94%</progress>
+    //                             <Link to={url} className="button is-large is-primary is-rounded">Apoyar</Link>
+    //                         </div>
+    //                     </div>
+    //                     {/* <!-- Hero footer: will stick at the bottom --> */}
+    //                     <div className="hero-foot">
+    //                         <nav className="tabs">
+    //                             <div className="container">
+    //                                 <ul>
+    //                                     <li className="is-active"><a>Overview</a></li>
+    //                                     <li><a>Modifiers</a></li>
+    //                                     <li><a>Grid</a></li>
+    //                                     <li><a>Elements</a></li>
+    //                                     <li><a>Components</a></li>
+    //                                     <li><a>Layout</a></li>
+    //                                 </ul>
+    //                             </div>
+    //                         </nav>
+    //                         <section className="box issue-body">
+    //                             <div className="container is-narrow">
+    //                                 <div className="content" dangerouslySetInnerHTML={{ __html: post.html }}>
+    //                                 </div>
+    //                             </div>
+    //                         </section>
+    //                     </div>
+    //                 </section>
+    //             </Layout>
+    //         </>
+    // )
 }
+
+const HeroBody = ({title, excerpt, host}) => (
+    <>
+        <h1 className="title is-spaced big-title has-text-white	">{title}</h1>
+        <h2 className="subtitle is-4-desktop is-5-mobile">{excerpt}</h2>
+        <h2 className="subtitle is-size-6-desktop is-size-5-mobile">Hosted by <strong>{host}</strong></h2>
+    </>
+)
 
 Issue.propTypes = {
     data: PropTypes.shape({
